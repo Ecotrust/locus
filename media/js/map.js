696e6167782155e090a627f41de7a2ca17a72a50
Ryan Hodges <rhodges@ecotrust.org>
2013-04-04 11:32:30 -0700

    Adding vector layers to maps

--- media/js/map.js
+++ media/js/map.js
@@ -1,4 +1,30 @@
+var map;
+
+
+function onPopupClose(evt) {
+    selectStoryPointControl.unselect(selectedFeature);
+    selectLocusControl.unselect(selectedFeature);
+}
+
+function onFeatureSelect(feature) {
+    selectedFeature = feature;
+    popup = new OpenLayers.Popup.FramedCloud("chicken", 
+                             feature.geometry.getBounds().getCenterLonLat(),
+                             null,
+                             "<div style='font-size:.8em'>Feature: " + feature.id +"<br>Area: " + feature.geometry.getArea()+"</div>",
+                             null, true, onPopupClose);
+    feature.popup = popup;
+    map.addPopup(popup);
+}
+
+function onFeatureUnselect(feature) {
+    map.removePopup(feature.popup);
+    feature.popup.destroy();
+    feature.popup = null;
+}    
+
 function mapInit() {
+    
     
     /*--- Click Control, from example: http://openlayers.org/dev/examples/click.html ---*/
     OpenLayers.Control.Click = OpenLayers.Class(OpenLayers.Control, {                
@@ -29,6 +55,20 @@ function mapInit() {
             // lonlat.transform(new OpenLayers.Projection("EPSG:900913"), new OpenLayers.Projection("EPSG:4326"));
             // alert("You clicked near " + lonlat.lat + ", " +
                                       // + lonlat.lon);
+                                      
+                                      
+            //TODO: Create vector layer (on init)
+            //TODO: Create new feature at e.xy
+            //TODO: Create popup for feature
+            //TODO: add feature to vector layer
+            //TODO: show feature
+            //TODO: show popup
+            //TODO: save feature with attributes from popup
+            //TODO: 
+            //TODO: 
+                                      
+                                      
+            /*
             var bubble = new OpenLayers.Popup.Anchored({
                 'id': 'new-story-point-bubble',
                 'lonlat': lonlat,
@@ -38,7 +78,22 @@ function mapInit() {
                 'closeBox': true
                 // 'closeBoxCallback': aFunction()
             });
-            bubble.show();
+            //bubble.show();
+            map.addPopup(bubble);
+            */
+            
+            /*
+            var popup = new OpenLayers.Popup.FramedCloud("popup",
+                lonlat,
+                null,
+                "<div style='font-size:.8em'>Feature: [id]<br>Foo: [attributes]</div>",
+                null,
+                true
+            );
+            
+            map.addPopup(popup);
+            // popup.show();
+            */
         }
 
     });
@@ -57,35 +112,74 @@ function mapInit() {
         center: new OpenLayers.LonLat(0, 0),
         zoom: 1
     };
-    var apiKey = "AvD-cuulbvBqwFDQGNB1gCXDEH4S6sEkS7Yw9r79gOyCvd2hBvQYPaRBem8cpkjv";
+    var bing_apiKey = "AvD-cuulbvBqwFDQGNB1gCXDEH4S6sEkS7Yw9r79gOyCvd2hBvQYPaRBem8cpkjv";
     var map_extent = new OpenLayers.Bounds(-20037508, -20037508, 20037508, 20037508.34);
-    var map = new OpenLayers.Map(null, {
+    
+    map = new OpenLayers.Map(null, {
       restrictedExtent: map_extent,
       displayProjection: new OpenLayers.Projection("EPSG:4326"),
-      // projection: new OpenLayers.Projection("EPSG:3857")
-      projection: new OpenLayers.Projection("EPSG:4326")
+      projection: new OpenLayers.Projection("EPSG:3857")
+      // projection: new OpenLayers.Projection("EPSG:4326")
     });
-    map.addControl(new OpenLayers.Control.LayerSwitcher());
-
+    
     /*
     var road = new OpenLayers.Layer.Bing({
         name: "Road",
-        key: apiKey,
+        key: bing_apiKey,
         type: "Road"
     });
     */
     var hybrid = new OpenLayers.Layer.Bing({
         name: "Hybrid",
-        key: apiKey,
+        key: bing_apiKey,
         type: "AerialWithLabels"
     });
     var aerial = new OpenLayers.Layer.Bing({
         name: "Aerial",
-        key: apiKey,
+        key: bing_apiKey,
         type: "Aerial"
     });
 
-    map.addLayers([aerial, hybrid]);
+    esriOcean = new OpenLayers.Layer.XYZ("ESRI Ocean", "http://services.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer/tile/${z}/${y}/${x}", {
+        sphericalMercator: true,
+        isBaseLayer: true,
+        numZoomLevels: 13,
+        attribution: "Sources: Esri, GEBCO, NOAA, National Geographic, DeLorme, NAVTEQ, Geonames.org, and others"
+    });
+    
+    
+    var storyPointLayer = new OpenLayers.Layer.Vector("Story Point Layer");
+    var locusLayer = new OpenLayers.Layer.Vector("Locus Layer");
+    
+    map.addLayers([aerial, hybrid, esriOcean, storyPointLayer, locusLayer]);
+    
+    map.addControl(new OpenLayers.Control.LayerSwitcher());
+    map.addControl(new OpenLayers.Control.MousePosition());
+    
+    var drawPointControls, drawLocusControls, selectStoryPointControl, selectLocusControl, selectedFeature;
+    
+    selectLocusControl = new OpenLayers.Control.SelectFeature(locusLayer,
+        {onSelect: onFeatureSelect, onUnselect: onFeatureUnselect});
+    selectStoryPointControl = new OpenLayers.Control.SelectFeature(storyPointLayer,
+        {onSelect: onFeatureSelect, onUnselect: onFeatureUnselect});
+    drawPointControls = {
+        point: new OpenLayers.Control.DrawFeature(storyPointLayer,
+                    OpenLayers.Handler.Point),
+        select: selectStoryPointControl
+    };
+    drawLocusControls = {
+        polygon: new OpenLayers.Control.DrawFeature(locusLayer,
+                    OpenLayers.Handler.Polygon),
+        select: selectLocusControl
+    };
+    
+    for(var key in drawPointControls) {
+        map.addControl(drawPointControls[key]);
+    }
+    
+    for(var key in drawLocusControls) {
+        map.addControl(drawLocusControls[key]);
+    }
     
     $('a[data-toggle="tab"]').on('shown',function(e) {
         
@@ -93,26 +187,41 @@ function mapInit() {
             dash_map_status = {
                 center: map.center,
                 zoom: map.zoom
-            }
+            };
+            drawPointControls['point'].deactivate();
+            selectStoryPointControl.deactivate();
         } else if (e.relatedTarget.id == "settings-tab") {
             set_map_status = {
                 center: map.center,
                 zoom: map.zoom
-            }
+            };
+            selectLocusControl.deactivate();
+            drawLocusControls['polygon'].deactivate();
+            drawLocusControls['select'].deactivate();
         } else if (e.relatedTarget.id == "world-tab") {
             other_map_status = {
                 center: map.center,
                 zoom: map.zoom
-            }
+            };
+            selectLocusControl.deactivate();
+            selectStoryPointControl.deactivate();
         }
         
         if (e.target.id == "dashboard-tab") {
+            drawPointControls['point'].activate();
+            selectStoryPointControl.activate();
+            $('#dash-map').show();
             map.render("dash-map");
             map.setCenter(dash_map_status.center, dash_map_status.zoom);
         } else if (e.target.id == "settings-tab" ) {
+            selectLocusControl.activate();
+            $('#your-locus').show();
             map.render("your-locus");
             map.setCenter(set_map_status.center, set_map_status.zoom);
         } else if (e.target.id == "world-tab" ) {
+            selectLocusControl.deactivate();
+            selectStoryPointControl.deactivate();
+            $('#loci-map').show();
             map.render("loci-map");
             map.setCenter(other_map_status.center, other_map_status.zoom);
         }
