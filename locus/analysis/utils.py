dafabafb64507f52a3b8e2f0d0cbfcfb6c764293
rhodges <rhodges@ecotrust.org>
2014-08-21 18:03:55 -0700

    fixing bug with pulling reports 'missing WorldMask'

--- locus/analysis/utils.py
+++ locus/analysis/utils.py
@@ -12,7 +12,7 @@ def get_terra_geom(bioregion):
     if report_cache_exists(bioregion, 'terra_geom'):
         terra_geom = get_report_cache(bioregion, 'terra_geom')
     else:
-        terra_mask = WorldMask.objects.get(id=1)
+        terra_mask = WorldMask.objects.all()[0]
         terra_geom = bioregion.output_geom.intersection(terra_mask.geometry)
         create_report_cache(bioregion, dict(terra_geom=terra_geom))
     return terra_geom
@@ -21,7 +21,7 @@ def get_oceanic_geom(bioregion):
     if report_cache_exists(bioregion, 'oceanic_geom'):
         oceanic_geom = get_report_cache(bioregion, 'oceanic_geom')
     else:
-        terra_mask = WorldMask.objects.get(id=1)
+        terra_mask = WorldMask.objects.all()[0]
         oceanic_geom = bioregion.output_geom.difference(terra_mask.geometry)
     return oceanic_geom
 
